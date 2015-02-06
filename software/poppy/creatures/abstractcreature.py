7b0775e61e64301fbffa74a9070cbdb5a9e207c7
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-02-06 16:11:15 +0100

    Add the possibility to not start the synchronization loops when creating a creature.

--- software/poppy/creatures/abstractcreature.py
+++ software/poppy/creatures/abstractcreature.py
@@ -14,7 +14,8 @@ class AbstractPoppyCreature(Robot):
 
     def __new__(cls,
                 config=None,
-                simulator=None, scene=None, host='127.0.0.1', port=19997, id=0):
+                simulator=None, scene=None, host='127.0.0.1', port=19997, id=0,
+                sync=True):
         """ Poppy Creature Factory.
 
         Creates a Robot (real or simulated) and specifies it to make it a specific Poppy Creature.
@@ -26,6 +27,7 @@ class AbstractPoppyCreature(Robot):
         :param str host: host of the simulator
         :param int port: port of the simulator
         :param int id: id of robot in the v-rep scene (not used yet!)
+        :param bool sync: choose if automatically starts the synchronization loops
 
         .. warning:: You can not specify a particular config when using a simulated robot!
 
@@ -61,11 +63,11 @@ class AbstractPoppyCreature(Robot):
 
             # TODO: use the id so we can have multiple poppy creatures
             # inside a single vrep scene
-            poppy_creature = from_vrep(config, host, port, scene)
+            poppy_creature = from_vrep(config, host, port, scene, sync=sync)
             poppy_creature.simulated = True
 
         else:
-            poppy_creature = from_json(config)
+            poppy_creature = from_json(config, sync)
             poppy_creature.simulated = False
 
         cls.setup(poppy_creature)
