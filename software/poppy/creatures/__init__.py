dfe4178b800fbc06a35e77fd69d5b65593058837
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-03-18 12:17:15 +0100

    Adds utility to function to get all installed poppy creatures using pip.

--- software/poppy/creatures/__init__.py
+++ software/poppy/creatures/__init__.py
@@ -1,16 +1,25 @@
+import sys
+import pip
+
 from abstractcreature import AbstractPoppyCreature
 
-try:
-    from poppy_humanoid import PoppyHumanoid
-except ImportError:
-    pass
 
-try:
-    from poppy_ergo import PoppyErgo
-except ImportError:
-    pass
+def installed_poppy_creatures_packages():
+    return [p.key for p in pip.get_installed_distributions()
+            if p.key.startswith('poppy-') and p.key != 'poppy-creature']
 
-try:
-    from poppy_ergo_jr import PoppyErgoJr
-except ImportError:
-    pass
+module = sys.modules[__name__]
+
+installed_poppy_creatures = {}
+
+for creature in installed_poppy_creatures_packages():
+    package = creature.replace('-', '_')
+    cls_name = ''.join(x.capitalize() or '_' for x in package.split('_'))
+
+    try:
+        cls = getattr(__import__(package), cls_name)
+        installed_poppy_creatures[creature] = cls
+        setattr(module, cls_name, cls)
+
+    except ImportError:
+        pass
